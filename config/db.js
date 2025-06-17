67e1247ba04f10263628d65e7294b14e9252328f
Coni <coti.garibotto@gmail.com>
2025-06-17 01:24:00 -0300

    UPDATE DB

--- config/db.js
+++ config/db.js
@@ -1,4 +1,28 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
+
+
+// Probar conexión
+sequelize.authenticate()
+  .then(() => console.log('✅ Conexión a la base de datos exitosa'))
+  .catch(err => console.error('❌ Error de conexión:', err));
+
+module.exports = sequelize;
+
+
+/*const { Sequelize } = require('sequelize');
 
 // Configuración de Sequelize
 const sequelize = new Sequelize('laboratorio2025', 'root', '', {
@@ -18,3 +42,4 @@ sequelize.authenticate()
   });
 
 module.exports = sequelize;
+*/
